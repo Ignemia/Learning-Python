b943da6bf63a75fca170dd9076de4227c8d29342
Adam Opalecký <adam@opalecky.cz>
2019-12-15 20:52:53 +0100

    First state of my squaring app
    Basic syntax and work with python

---
+++ squaring_app.py
@@ -0,0 +1,10 @@
+# This is how you make comments
+# Haven't yet figured out how to effectively make multi-line comments - will work on that
+# def is something like "function" in javascript
+# weird Python doesn't use curly braces
+# I would love to figure out how to force types for function parameters
+def square(a):
+    # wow... this actually works... Python is easy
+    return print(a*a)
+
+square(5)
