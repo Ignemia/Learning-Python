cab5e6f9423b4c9c1b5bf8ce1de4b91ee9d9c1be
Adam Opalecký <adam@opalecky.cz>
2019-12-15 20:57:53 +0100

    I broke it
    But I need to document it to learn from my mistakes

--- squaring_app.py
+++ squaring_app.py
@@ -3,8 +3,13 @@
 # def is something like "function" in javascript
 # weird Python doesn't use curly braces
 # I would love to figure out how to force types for function parameters
+# I should get people to input. I need to get the grip of handleing user inputs
 def square(a):
     # wow... this actually works... Python is easy
     return print(a*a)
 
-square(5)
+# this is weird you define variables without declaring either whether it is variable or constant for as much as I know atm. Not even datatypes. This feels off
+user_input = input()
+# this is simple. Method input is the only thing you need to get user to input into terminal.
+square(user_input)
+# ah s#17 doens't even work for numbers what is wrong
