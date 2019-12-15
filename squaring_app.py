ffd4710bd152db1867a470503149db474e4a87eb
Adam Opalecký <adam@opalecky.cz>
2019-12-15 21:26:52 +0100

    UX improvements ( ͡° ͜ʖ ͡°)
    Allowed users to quit and also indefinitely cycled the app

--- squaring_app.py
+++ squaring_app.py
@@ -1,34 +1,43 @@
-# This is how you make comments
-# Haven't yet figured out how to effectively make multi-line comments - will work on that
-# def is something like "function" in javascript
-# weird Python doesn't use curly braces
-# I would love to figure out how to force types for function parameters
-# I should get people to input. I need to get the grip of handleing user inputs
+end = False
+# and I almost did it again. Damn you semicolons
 def square(a):
-    # well this is not the way
-    #a = (int) a
+    # That worked?
+    global end
+    # K boomer
+    # ffs... || doesn't work in this language
+    #if (a == 'quit') || (a == 'Quit'):
 
-    # somehow this is how you are supposed to cast types in python. I don't like it.
-    # Well... it throws errors if I input characters or string into terminal. Will look into that and figure out try catch blocks.
-    # let's see
-    try:
-        # The syntax is really demanding. One tab of and code doesn't work. It's getting on my nerves
-        a = int(a)
-    # why on earth is this not working?
-    #catch:
+    if (a == 'quit') or (a == 'Quit'):
+        # well this sucks...
+        # I just tried this type of code. Doesn't make the game stop. I'll try to print out what value this has. And depending on that answer just google.
+        end = True
+        # print here gives me True. Which means that it is not having any effect on the global variable @end
+        #print(end)
+    else:
+        # seriously the tabbind drives me crazy. Just use semicolons like any other language
+        try:
+            a = int(a)
+            # i moved it here so I can call new input after.
+            return print(a*a)
 
-    # Because there is no catch in python. It's except. WHY TF IS IT EXCEPT INSTEAD OF CATCH. 
-    except:
-        return print("Try number next time.")
+        except:
+            return print("Try number next time.")
+#I'll want to call this function recursively after end of every iteration of the same function so it is in a cycle and I do not need to restart it all the time.
+def __main__():
+    print("Give me a number to square or type 'quit' to exit the app")
+    user_input = input()
+    square(user_input)
+    # Exactly as I said. No effect on the global variable. I'll try the same thing that I know from javascript.
+    # Use the keyword Global to say that I want to effect the variable globally 
+    #print(end)
 
-    return print(a*a)
+    # I tried to use short syntax to test @end but it doesn't work like that obviously
+    #if !(end):
 
-# this is weird you define variables without declaring either whether it is variable or constant for as much as I know atm. Not even datatypes. This feels off
-user_input = input()
-# this is simple. Method input is the only thing you need to get user to input into terminal.
-square(user_input)
-# ah s#17 doens't even work for numbers what is wrong
-# The error says that it doesn't work because I'm giving it string even tho I am using the number 5 as input. Why does it do that?
-# I'll try to cast it
-
-#Next commit I'll remove all the comments to clear out a lil' bit of space.
+    # Somehow intuitively I figured this out on the first try
+    if not(end):
+        __main__()
+# It works!
+# I should add a cycle break keyword. Because now I have to stop it manually. 
+# My optimistic plan is to define a boolean variable that will store whether user used the word "quit" instead of real input. Which will tell me whether to quit or not...
+__main__()
